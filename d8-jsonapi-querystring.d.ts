827e55d54c200f0be8fec4a1664932904cc6d17a
Joao Garin <joao.garin@gmail.com>
2017-07-01 00:04:30 +0100

    added sortedCreated

--- d8-jsonapi-querystring.d.ts
+++ d8-jsonapi-querystring.d.ts
@@ -1,6 +1,7 @@
 
 declare module "d8-jsonapi-querystring" {
     interface jsonApiRequestObject {
+        sortCreated?: any,
         sort?: any,
         fields?: any,
         include?: string[],
