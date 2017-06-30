de86614fe781380b8ab951a4a1815a3cab45222b
Joao Garin <joao.garin@gmail.com>
2017-07-01 00:14:06 +0100

    Revert "added sortedCreated"
    This reverts commit 827e55d54c200f0be8fec4a1664932904cc6d17a.

--- d8-jsonapi-querystring.d.ts
+++ d8-jsonapi-querystring.d.ts
@@ -1,7 +1,6 @@
 
 declare module "d8-jsonapi-querystring" {
     interface jsonApiRequestObject {
-        sortCreated?: any,
         sort?: any,
         fields?: any,
         include?: string[],
